a4a3e876f76f10d495c4f3b8680c5b1ec94caa3a
mayankksinha1 <mayankksinha1@gmail.com>
2024-02-06 01:15:05 +0530

    corrected format

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function datemks() {
-    return new Date();
+    const currentDate = new Date();
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return currentDate.toLocaleDateString(undefined, options);
 }
 
 module.exports = datemks
